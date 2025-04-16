0af71150342fe697e026305773bef1c16a06088d
hoangan137 <an506840@gmail.com>
2025-04-16 11:05:03 +0700

    update

--- ASC.Web/Data/Migrations/20250416034407_Remove_Demo_HAn_Lab2.Designer.cs
+++ ASC.Web/Data/Migrations/20250416034407_Remove_Demo_HAn_Lab2.Designer.cs
@@ -4,6 +4,7 @@ using ASC.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ASC.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250416034407_Remove_Demo_LeHuuTu_Lab2")]
+    partial class Remove_Demo_LeHuuTu_Lab2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -41,6 +44,30 @@ namespace ASC.Web.Data.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Books");
+                });
+
+            modelBuilder.Entity("ASC.Model.Models.Demo_LeHuuTu_Lab2", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                    b.Property<DateTime>("CreatedAt")
+                        .HasColumnType("datetime2");
+
+                    b.Property<string>("Description")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Demo_Lab2_LeHuuTu");
                 });
 
             modelBuilder.Entity("ASC.Model.Models.MasterDataKey", b =>
--- ASC.Web/Data/Migrations/20250416035308_RemoveDemoTable.cs
+++ ASC.Web/Data/Migrations/20250416035308_RemoveDemoTable.cs
@@ -6,44 +6,31 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace ASC.Web.Data.Migrations
 {
     /// <inheritdoc />
-    public partial class Demo_Lab2_LeHuuTu : Migration
+    public partial class RemoveDemoTable : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "Demo_Lab2_LeHuuTu");
+        }
 
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
             migrationBuilder.CreateTable(
                 name: "Demo_Lab2_LeHuuTu",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Demo_Lab2_LeHuuTu", x => x.Id);
-                });
-        }
-
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-
-            migrationBuilder.CreateTable(
-                name: "Demo_LeHuuTus",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
                     Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Demo_LeHuuTus", x => x.Id);
+                    table.PrimaryKey("PK_Demo_Lab2_LeHuuTu", x => x.Id);
                 });
         }
     }
